f2e1d7fd2e545151c3dce9b45aba23e2752c0d5d
Brett Petersen <bap034@gmail.com>
2023-08-28 21:42:10 -0700

    Update mischief_season_challenge.py indentation error

--- mischief_season_challenge.py
+++ mischief_season_challenge.py
@@ -239,7 +239,7 @@ class MischiefSlack:
                 reset_scores()
                 send_debug_message("Resetting leaderboard")
                 count += 1
-                if '!createDB' in self._lower_text and self._user_id == 'U03MGMXE5U6':
+            if '!createDB' in self._lower_text and self._user_id == 'U03MGMXE5U6':
                 # to_print = collect_stats(3, True)
                 # send_tribe_message(to_print, channel=self._channel, bot_name=self._name)
                 # reset_scores()
