88d87489d50d8263d9984e7e2aa06253a7a8b037
Brett Petersen <bap034@gmail.com>
2023-08-29 16:43:05 -0700

    Update mischief_season_challenge.py debugging

--- mischief_season_challenge.py
+++ mischief_season_challenge.py
@@ -242,11 +242,13 @@ class MischiefSlack:
                 reset_scores()
                 send_debug_message("Resetting leaderboard")
                 count += 1
-            if '!createDB' in self._lower_text and self._user_id == adminSlackId:
+            if '!createDB' in self._lower_text: #and self._user_id == adminSlackId:
                 # to_print = collect_stats(3, True)
                 # send_tribe_message(to_print, channel=self._channel, bot_name=self._name)
                 # reset_scores()
-                send_debug_message("Creating DB")
+                send_debug_message("Creating DB +++")
+                send_message("Creating DB ...", channel=self._channel, bot_name=self._name, url=self._avatar_url)
+                print("Creating DB ---
                 # groupInfo = get_group_info()
                 # print("groupInfo: ", groupInfo)
                 # create_db(groupInfo)
