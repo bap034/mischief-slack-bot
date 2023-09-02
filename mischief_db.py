530d025d53def22c1e294d54f0a15f0dd665db24
Brett Petersen <bap034@gmail.com>
2023-09-02 00:12:07 -0700

    Update mischief_db.py debug

--- mischief_db.py
+++ mischief_db.py
@@ -48,7 +48,7 @@ def create_new_db_v2(member_info):
         cursor = sqlConnection.cursor()
     
         print("Dropping existing DB: ", __table_name__)
-        cursor.execute(sql.SQL("DROP TABLE IF EXISTS %s"), (__table_name__, )) # Requires the empty tuple: https://stackoverflow.com/a/50360509
+        cursor.execute(sql.SQL("DROP TABLE IF EXISTS %s"), [__table_name__]) # Requires the empty tuple: https://stackoverflow.com/a/50360509
         print("Successfully dropped existing DB: ", __table_name__)
         
         print("Creating new DB v2: ", __table_name__)
@@ -71,7 +71,7 @@ def create_new_db_v2(member_info):
               last_time BIGINT
           )
           """), 
-            (__table_name__, ) # Requires the empty tuple: https://stackoverflow.com/a/50360509
+            [__table_name__] # Requires the empty tuple: https://stackoverflow.com/a/50360509
         )
         print("Successfully created new DB: ", __table_name__)
     
