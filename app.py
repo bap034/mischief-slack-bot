8c412d2276ff0e002eba54692b9d2416359c7de5
Brett Petersen <bap034@gmail.com>
2023-09-03 15:30:06 -0700

    Update app.py webhook printing

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ def webhook():
     print("event received")
     BOT_CHANNEL = "C03UHTL3J58"
     data = request.get_json()
+    print("Webhook data: ", data)
+    print("Request Environment: ", request.__dict__['environ'])
     if data['type'] == "url_verification":
         return jsonify({'challenge': data['challenge']})
     print('HTTP_X_SLACK_RETRY_NUM' in list(request.__dict__['environ'].keys()))
