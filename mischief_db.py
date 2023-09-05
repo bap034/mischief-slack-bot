0ea2d95b1da456559a38828e53c1e84d41e522f7
Brett Petersen <bap034@gmail.com>
2023-09-04 19:07:03 -0700

    Update mischief_db.py debug

--- mischief_db.py
+++ mischief_db.py
@@ -291,7 +291,7 @@ def add_to_db(channel_id, names, addition, lift_num, cardio_num, sprint_num, thr
             cursor.execute(sql.SQL(
                 "SELECT score FROM mischief_data WHERE slack_id = %s"), [str(ids[x])])
             score = cursor.fetchall()[0][0]
-            score = int(score)
+            score_val = int(score)
             if score != -1:
                 updateCommand = """
                     UPDATE {table_name} SET
