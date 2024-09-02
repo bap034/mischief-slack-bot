bb7817c0408776bd32effa8116c73903e47f4242
Brett Petersen <bap034@gmail.com>
2024-09-01 23:06:03 -0700

    Update mischief_db.py fixed compile error

--- mischief_db.py
+++ mischief_db.py
@@ -152,7 +152,7 @@ def fill_table_v2(member_info):
             realName = member["profile"]["real_name"] # Note: deleted users do not have a `member["real_name"]` value but all users have a `profile` with a `real_name`.
 
             # Because beth's chris' name has a rich text apostrophe
-            if member['id] == 'U075A6KJE9G': 
+            if member['id'] == 'U075A6KJE9G': 
                 realName = 'Steakhouse'
             
             print("Member real_name: ", realName)
