673af375f65d74a73911b2aae9d3263a9a730bea
Brett Petersen <bap034@gmail.com>
2024-09-01 21:01:24 -0700

    Update mischief_db.py debug get_group_info

--- mischief_db.py
+++ mischief_db.py
@@ -299,7 +299,7 @@ def collect_stats(datafield, rev):
 #     except (Exception, psycopg2.DatabaseError) as error:
 #         send_debug_message(error)        
 
-def get_group_info(print_values=False):
+def get_group_info(print_values):
     url = "https://slack.com/api/users.list"
     json = requests.get(url, headers=__auth__).json()
     if print_values:
